09963ab11019bd78c7b934fd66b19b3609d799c7
Ankit Mishra <ankit1703@hotmail.com>
2019-05-12 15:23:52 +0530

    All test case passed.

---
+++ Practice/BasicProgramming/Ladderophilia/Ladderophilia.cpp
@@ -0,0 +1,20 @@
+// Ladderophilia.cpp 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n{ 0 };
+	cin >> n;
+
+	cout << "*   *" << endl;
+	cout << "*   *" << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "*****" << endl;
+		cout << "*   *" << endl;
+		cout << "*   *" << endl;
+	}
+}
+
