29e11f6bdbd042c06ab8edb9028ac5a11255d751
miank <ankit1703@gmail.com>
2020-04-08 17:00:00 +0530

    All test case passed

---
+++ Practice/BasicProgramming/IAmEasy/IAmEasy.cpp
@@ -0,0 +1,17 @@
+// IAmEasy.cpp 
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	long long n(0);
+	cin >> n;
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << n * i << endl;
+	}
+}
