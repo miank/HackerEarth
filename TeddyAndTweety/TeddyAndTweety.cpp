52c6e6d8571c060561c72bc1f8fb7543cbb59111
miank <ankit1703@gmail.com>
2020-04-20 11:04:39 +0530

    All test case passed.

---
+++ TeddyAndTweety/TeddyAndTweety.cpp
@@ -0,0 +1,21 @@
+// TeddyAndTweety.cpp 
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int n(0);
+	cin >> n;
+
+	if (n % 3 == 0)
+	{
+		cout << "YES" << endl;
+	}
+	else
+	{
+		cout << "NO" << endl;
+	}
+	return 0;
+}
